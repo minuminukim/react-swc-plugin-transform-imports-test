e969669350e49f30ed6fa5835c6059ef16ea8b2e
Minu Kim <minumoseskim@gmail.com>
2023-05-07 08:34:23 -0400

    test named imports

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## 
+# import cost test in a react app built with webpack and swc
+
+## named imports
+
+```js
+// App.tsx
+
+import { DirectionsRun } from '@mui/icons-material';
+import { flatten } from 'lodash';
+```
+
+<img src="./docs/named-import.png" width="500" />
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
+import { DirectionsRun } from '@mui/icons-material';
+import { flatten } from 'lodash';
 
-const App = () => (
-  <div>
-    <h1>React Webpack SWC</h1>
-  </div>
-);
+const App = () => {
+  const [showList, setShowList] = React.useState(false);
+  return (
+    <div>
+      <h1>React Webpack SWC</h1>
+      <DirectionsRun onClick={() => setShowList(true)} />
+      {showList && (
+        <ul>
+          {flatten([[1], [2], 3]).map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default App;
